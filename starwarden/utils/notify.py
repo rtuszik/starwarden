0abd87342875ecf8bc40721158cf755b5bfd3618
Robin Tuszik <mail@robin.gg>
2025-06-29 13:13:32 +0200

    add notification module

---
+++ starwarden/utils/notify.py
@@ -0,0 +1,28 @@
+from utils import get_logger
+import apprise
+
+logger = get_logger
+
+
+
+def send_notification(config: dict, message: str, title: str = "Starwarden Notification"):
+    apprise_urls = config.get("APPRISE_URLS")
+    if not apprise_urls:
+        logger.info("No APPRISE_URLS set, skipping notification.")
+        return
+
+    apobj = apprise.Apprise()
+
+    for url in apprise_urls.split(','):
+        if url.strip():
+            apobj.add(url.strip())
+
+    if not apobj.servers:
+        logger.warning("No valid Apprise URLs were found after processing the APPRISE_URLS variable.")
+        return
+
+    if not apobj.notify(body=message, title=title):
+        logger.error("Failed to send notification to one or more Apprise targets.")
+    else:
+        logger.info("Successfully sent notification.")
+
